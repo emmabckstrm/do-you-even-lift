66fa1f7dc566773b1a566d35b4205ee459d9f2d2
DESKTOP-C4HHKOL\Jayway Stockholm <emba93@live.se>
2018-01-26 17:15:01 +0100

    Trying to format logging

--- Assets/Scripts/InteractableObjectTrackMovement.cs
+++ Assets/Scripts/InteractableObjectTrackMovement.cs
@@ -2,6 +2,7 @@
 {
     using UnityEngine;
     using System.IO;
+    using System.Linq;
 
     public class InteractableObjectTrackMovement : VRTK_InteractableObject
     {
@@ -20,16 +21,16 @@
         private float lastSpeed = 0f;
         private Vector3 lastPosition = Vector3.zero;
         protected string serializedData;
-        protected StreamWriter writer;
-        private float timestamp = 0.0f;
+        //static protected StreamWriter writer = new StreamWriter("MyPath.txt", true);
+        private float timeGrabStart = 0.0f;
         private float timeGrabbed = 0.0f;
         private int numberOfGrabs = 0;
         private int numberOfTouches = 0;
+        public string path = "Assets/Logs/log.txt";
 
         // Use this for initialization
         protected void Start()
         {
-            //writer = new StreamWriter("MyPath.txt", true);
             Rigidbody rb = interactableRigidbody;
             // Calculates movement limit depending on what movementLimitationType is chosen
             if (movementLimitType == MovementLimitationTypes.VelocityAnyDirection || movementLimitType == MovementLimitationTypes.VelocityVertical)
@@ -75,19 +76,16 @@
         {
             return (((transform.position - lastPos).magnitude) / Time.deltaTime);
         }
-
         // Calculates movement velocity in vertical
         private float CalculateVerticalVelocity(Vector3 lastPos)
         {
             return (Mathf.Abs(transform.position.y - lastPos.y)) / Time.deltaTime;
         }
-
         // Calculates the acceleration in any direction
         private float CalculateAccelerationAny(Vector3 lastPos)
         {
             return (CalculateGeneralVelocity(lastPos) / Time.deltaTime);
         }
-
         // Calculates the acceleration in vertical direction
         private float CalculateAccelerationVertical(Vector3 lastPos)
         {
@@ -109,40 +107,60 @@
                 ForceReleaseGrab();
             }
         }
-
         // Overridden to log start time of grab
         public override void Grabbed(VRTK_InteractGrab currentGrabbingObject = null)
         {
-            Debug.Log("Yey, it was grabbed! It's grabbed by " + currentGrabbingObject.name);
-            timestamp = Time.time;
+            //Debug.Log("Yey, it was grabbed! It's grabbed by " + currentGrabbingObject.name);
+            timeGrabStart = Time.time;
             base.Grabbed(currentGrabbingObject);
         }
-
         // Overridden to log end time of grab
         public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject = null)
         { 
-            timeGrabbed = Time.time - timestamp;
+            timeGrabbed = Time.time - timeGrabStart;
             numberOfGrabs += 1;
-            Debug.Log("Ungrabbed! " + previousGrabbingObject.name + " after this time " + timeGrabbed);
-            Debug.Log(this.name + " Total grabs " + numberOfGrabs);
+            //Debug.Log("Ungrabbed! " + previousGrabbingObject.name + " after this time " + timeGrabbed);
+            //Debug.Log(this.name + " Total grabs " + numberOfGrabs);
+            writeString2("hej-----" + numberOfGrabs);
             base.Ungrabbed(previousGrabbingObject);
         }
-
+        //Overridden to count number of touches to object
         public override void StartTouching(VRTK_InteractTouch currentTouchingObject)
         {
             numberOfTouches += 1;
-            Debug.Log("total touches to this obj: " + numberOfTouches);
+            //Debug.Log("total touches to this obj: " + numberOfTouches);
             base.StartTouching(currentTouchingObject);
         }
-
-        static void writeString(string str)
+        // Writes to file 
+        public void writeString(string str)
         {
-            string path = "MyPath.txt";
+            
+            //Write some text to the file
+            StreamWriter writer = new StreamWriter(path);
+            long endPoint = writer.BaseStream.Length;
+            writer.BaseStream.Seek((endPoint - 1), SeekOrigin.Begin);
+            writer.WriteLine(str);
+            writer.Close();
+        }
+        public void writeString2(string str) {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                long endPoint = fs.Length;
+                // Set the stream position to the end of the file.        
+                fs.Seek(endPoint-2, SeekOrigin.Begin);
+                sw.Write(str + "\n\n}");
+                sw.Flush();
+            }
+        }
+        public void CreateEntry(string npcName) //npcName = "item1"
+        {
+            //string endTag = String.Format("[/{0}]", npcName);
+            string lineToAdd = "//Add a line here in between the specific boundaries";
 
-            //Write some text to the test.txt file
-            StreamWriter writer = new StreamWriter(path, true);
-            //writer.WriteLine("TEST");
-            //writer.Close();
+            var txtLines = File.ReadAllLines(path).ToList();   //Fill a list with the lines from the txt file.
+            //txtLines.Insert(txtLines.IndexOf(endTag), lineToAdd);  //Insert the line you want to add last under the tag 'item1'.
+            //File.WriteAllLines(path, txtLines);                //Add the lines including the new one.
         }
 
 
